0201b50640f77866e33a573529551bec168f3e9e
Ethan Davidson <theejdavidson@gmail.com>
2020-01-25 01:13:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
   {
 	  :railroads => 
 	  {:pieces => 4,
-	  :rent_in_dollars => {:one => 1, :two => 2, :three => 3, :four => 4},
+	  :rent_in_dollars => {:one_piece_owned => 25, :two => 2, :three => 3, :four => 4},
 	  :names => {:one => 1, :two => 2, :three => 3, :four => 4}
 	  }
 	}
